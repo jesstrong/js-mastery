7f49abc8ee3ef4efd421ce0df0b8aa27d4b40ec7
JES Strong <janetestrong@yahoo.com>
2021-03-20 19:09:33 -0400

    added some html for layout of donut clicker game

--- src/js/DonutShop.js
+++ src/js/DonutShop.js
@@ -5,20 +5,38 @@ class DonutShop {
          //Need auto clickers count
     this.AutoClickerCount = 0; 
          //Need auto clicker cost     
-    this.AutoClickerCost = 30; //verify what sh cost be?? 
+    this.AutoClickerCost = 20; //verify what sh cost be?? 
          //Need donut multiplier (how many we have - count)
     this.DonutMultiplierCount = 0;
          //have a donut multiplier cost
     this.DonutMultiplierCost = 100; 
+
+        //costs for auto clickers and multipliers - need methods for these
+    this.Multiplier = 1.01; 
+    this.AutoPriceUp = 1.05;
+    this.MultiplierPriceUp = 1.1;
     
     }     
-    
-        
+            
     getTotalDonutsCount(){
         return this.TotalDonuts; 
     }
+    
+    getAutoClickerCount(){
+        return this.AutoClickerCount;
+    }
 
+    getAutoClickerCost(){
+        return this.AutoClickerCost; 
+    }
 
+    getDonutMultiplierCount(){
+        return this.DonutMultiplierCount;
+    }
+
+    getDonutMultiplierCost(){
+        return this.DonutMultiplierCost; 
+    }
 
 
 
