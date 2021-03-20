37aaff9b39f46d8b74e77fee63ad369960033490
JES Strong <janetestrong@yahoo.com>
2021-03-20 17:55:18 -0400

    added attributes to DS class and method get donuts

---
+++ src/js/DonutShop.js
@@ -0,0 +1,28 @@
+class DonutShop {
+    constructor(){
+        //Need donut count (part of Feature 1)
+    this.TotalDonuts = 0; 
+         //Need auto clickers count
+    this.AutoClickerCount = 0; 
+         //Need auto clicker cost     
+    this.AutoClickerCost = 30; //verify what sh cost be?? 
+         //Need donut multiplier (how many we have - count)
+    this.DonutMultiplierCount = 0;
+         //have a donut multiplier cost
+    this.DonutMultiplierCost = 100; 
+    
+    }     
+    
+        
+    getTotalDonutsCount(){
+        return this.TotalDonuts; 
+    }
+
+
+
+
+
+
+
+    
+}
