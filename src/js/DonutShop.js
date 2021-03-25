5d85591509d193f350734b18c5a6bec443882ef4
JES Strong <janetestrong@yahoo.com>
2021-03-25 19:24:18 -0400

    added multiplier and modal functions and mutli cost

--- src/js/DonutShop.js
+++ src/js/DonutShop.js
@@ -1,13 +1,13 @@
 class DonutShop {
     constructor() {
        this.DonutCount = 0;
-       this.AutoDonutClickerCost = 100; //verify this amount
+       this.AutoDonutClickerCost = 100;
        this.AutoDonutClickerCount = 0; 
        this.DonutMultiplierCount = 0; 
-       this.DonutMultiplierCost = 10;  //verify this amount
+       this.DonutMultiplierCost = 10;  
        this.IntervalID = undefined; 
        this.DonutsEarned = 1;  
-
+  
     }
      
     getDonutCount(){
@@ -27,106 +27,54 @@ class DonutShop {
     }
 
     getDonutMultiplierCost(){
-        return this.getDonutMultiplierCost; 
+        return this.DonutMultiplierCost; 
     }
 
     getDonutsEarned(){
-        return this.DonutsEarned; 
+        return this.DonutsEarned * this.GetDonutMultiplier();
     }
 
     AddDonuts = () => {
-        this.DonutCount += this.DonutsEarned; 
+        this.DonutCount += this.getDonutsEarned(); 
+        UpdateText();
     }
     
     AddAuto = () => {
-       this.AutoDonutClickerCount++; 
-       if(this.getDonutCount -= this.getAutoDonutClickerCost) {
-          this.Interval = SetInterval(ProcessAutoclicker, 1000)
-       }else {
-        this.Interval = undefined;this.IntervalID; 
+        console.log(this.DonutCount);
+        console.log(this.AutoDonutClickerCost);
+        if(this.DonutCount >= this.AutoDonutClickerCost) {
+            this.DonutCount -= this.AutoDonutClickerCost
+            this.AutoDonutClickerCount++; 
+            if(this.IntervalID == undefined){
+            console.log ("starting interval");
+                this.IntervalID = setInterval(this.ProcessAutoClicker, 1000);
+                console.log (this.IntervalID);
+            }
+           this.AutoDonutClickerCost += (.1 *this.AutoDonutClickerCost); 
+                  
        }
-    }   
-       //this.DonutCount -=this.DonutMultiplierCost; 
-       //this.Interval = SetInterval(ProcessAutoclicker, 1000)
-       //if (this.Interval == undefined)this.IntervalID; 
-
-
-       //this.DonutCount -= this.AutoDonutClickerCost; 
-       //code:   this.Interval = SetInterval(ProcessAutoclicker, 1000)
-       //this code will be after you check to see if they have enough donuts to buy Autoclicker, when set interval code is:   
-       //need if stmt (donutcount is greater than the cost ) stmt sh ensure that they have enough donuts 
-       //                if this.Interval = undefined then start the Interval; 
-  //}  
-
-    AddDonutMulti = () => {
-        this.DonutMultiplierCount++; 
-        if (this.DonutCount -= this.DonutMultiplierCost) {
-            this.DonutsEarned = Math.pow(this.DonutsEarned, this.DonutMultiplierCount);
-            
-
-        //need if stmt (donutcount is greater than the cost ) stmt sh ensure that they have enough donuts 
-        //next 2 lines will be inside if stmt 
+    }        
         
-        //if (this.DonutCount -= this.DonutMultiplierCost)
-        //this.DonutsEarned = Math.pow(this.DonutsEarned, this.DonutMultiplierCount);
+    AddDonutMulti = () => {
+        if(this.DonutCount >= this.DonutMultiplierCost) {
+            this.DonutCount -= this.DonutMultiplierCost
+            this.DonutMultiplierCount++; 
+            this.DonutMultiplierCost += (.1 *this.DonutMultiplierCost); 
+        }
+        console.log(this.DonutMultiplierCount);
+        console.log(this.DonutMultiplierCost);
+                  
+       }
+           
+    GetDonutMultiplier = () => {
+        return Math.pow(1.1, this.DonutMultiplierCount);
     }
-
+      
     ProcessAutoClicker = () => {
         for (var i = 0; i < this.AutoDonutClickerCount; i++){
             this.AddDonuts();
+         
         }
-
-    }
+    }   
+  
 }
-
-    /*buyAutoDonutClicker(){
-        this.autoDonutClickerCount += 1; 
-        this.subtractAutoDonutClickerCostFromAutoDonutClickerCount();
-        this.increaseAutoDonutClickerCost();
-    }
-
-    
-    increaseAutoDonutClickerCost(){
-        this.autoDonutClickerCost += this.autoDonutClickerCost * 0.1; 
-    }
-
-    checkClickCountToBuyAutoDonutClicker(){
-        if(this.notEnoughClicksToPurchaseAutoDonutClicker()){
-            return null;
-        } else {
-            this.buyAutoDonutClicker();
-        } 
-    }
-
-    turnOnAutoDonutClicker(){
-        for (let i = 0; i < this.autoDonutClickerCount; i++){
-            this.clickForDonut();
-        }
-    }
-    
-    purchaseDonutMultiplier(){
-        this.donutMultiplierCount += 1;
-        this.subtractDonutMultiplierCostFromDonutCount();
-        this.increaseDonutMultiplierCost();
-    }
-
-    
-    increaseDonutMultiplierCost(){
-        this.donutMultiplierCost += this.donutMultiplierCost * 0.1;
-    }
-
-    
-
-    checkClickCountToBuyDonutMultiplier() {
-        if(this.notEnoughClicksToPurchaseDonutMultiplier()){
-            return null;
-        } else {
-            this.buyDonutMultiplier;
-        } 
-    }
-
-    calculateDonutMultiplierValue(){
-        return Math.pow(1.2, this.donutMultiplierCount);
-    }*/
-
-
--- src/js/app.js
+++ src/js/app.js
@@ -1,60 +1,3 @@
-var myDS = new DonutShop();
-
-const donutCount = document.getElementById("donutCount");
-const autoCountText = document.getElementById("autoCountText");
-const multCountText = document.getElementById("multCountText");
-const autoText = document.getElementById("autoText");
-const multText = document.getElementById("multText");
-
-let clickButton = document.getElementById("clickButton");
-let autoButton = document.getElementById("autoClickButton");
-let multButton = document.getElementById("multButton");
-
-clickButton.addEventListener('click', ClickButton);
-autoButton.addEventListener('click', );
-//multButton.addEventListener('click', AddMultiplier);
-
-function ClickButton(){
-    myDS.AddDonuts();
-    UpdateText();
-}
-
-function UpdateText(){
-    donutCount.innerText = "Donuts" + Math.floor(myDS.getDonutCount());
-   // autoCountText.innterText = "Donut AutoClicker" + myDS.getAutoDonutClickerCount();
-   // multCountText.innerText = "Status Multiplier" + myDS.getDonutMultiplier() + "x";
-   // autoText.innerText = "Auto Donutclicker Cost" + myDS.getAutoDonutClickerCost() + "donuts";
-   // multText.innerText = "Multiplier Cost" + myDS.getDonutMultiplierCost() + "donuts";
-}
-/*function AddClicker(){
-    if(myDS.DonutCount >= myDS.getAutoDonutClickerCost()){
-        myDS.AddDonuts(-myDS.getAutoDonutClickerCost());
-        myDS.AddAuto();
-        UpdateText();
-    }
-
-function AddMultiplier(){
-    if(myDS.donutCount >= myDS.getDonutMultiplierCost());
-        myDS.AddDonuts(-myDS.getDonutMultiplierCost());
-        myDS.AddDonutMulti();
-        UpdateText();
-    }
-}
-
-
-UpdateText();
-
-var myVar = setInterval(TimedAuto, 100);
-function TimedAuto(){
-    myDS.DonutCount = myDS.AddDonuts(myDS.getAutoDonutClickerCount() * myDS.getDonutMultiplier()/10); // NEED to change amount
-    UpdateText();
-}
-
-
-
-}*/
-
-
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -77,6 +20,78 @@ span.onclick = function() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
+      
     modal.style.display = "none";
   }
 }
+
+
+var myDS = new DonutShop();
+
+// DECLARE VARIABLE FOR ELEMENTS 
+const donutCount = document.getElementById("donutCount");
+const autoCountText = document.getElementById("autoCountText");
+const autoClickerCost = document.getElementById("autoClickerCost");
+const multiplierCount = document.getElementById("multiplierCount");
+const multipleCost = document.getElementById("multipleCost");
+
+// CLICK BUTTONS 
+let clickButton = document.getElementById("clickButton");
+let autoClickButton = document.getElementById("autoClickButton");
+let multiButton = document.getElementById("multiButton");
+let resetButton = document.getElementById("resetButton");
+
+// EVENT LISTENERS passing in functions 
+clickButton.addEventListener('click', ClickButton);
+autoClickButton.addEventListener('click', AutoClickButton);
+multiButton.addEventListener('click', MultiButton);
+resetButton.addEventListener('click', ResetGame);
+
+// FUNCTIONS for EVENT LISTENERS
+function ClickButton(){
+    myDS.AddDonuts();
+    UpdateText();
+}
+function AutoClickButton(){
+    console.log("buy auto clicker");
+    myDS.AddAuto();
+    UpdateText();
+}
+
+function MultiButton(){
+    console.log("buy multiplier");
+    myDS.AddDonutMulti();
+    UpdateText();
+}
+
+//FUNCTIONS FOR 
+function UpdateText(){
+    donutCount.innerText = "Donuts" + Math.floor(myDS.getDonutCount());
+    autoCountText.innerText = "Donut AutoClicker Count" + myDS.getAutoDonutClickerCount();
+    autoClickerCost.innerText = "Auto Donutclicker Cost" + myDS.getAutoDonutClickerCost() + "donuts";
+    multiplierCount.innerText = "Status Multiplier" + myDS.GetDonutMultiplier() + "x";
+    multipleCost.innerText = "Multiplier Cost" + myDS.getDonutMultiplierCost() + "donuts";
+}
+function AddClicker(){
+    if(myDS.DonutCount >= myDS.getAutoDonutClickerCost()){
+      myDS.AddDonuts(-myDS.getAutoDonutClickerCost());
+      myDS.AddAuto();
+      UpdateText();
+    }
+}
+
+function ResetGame(){
+    myDS = new DonutShop();
+    UpdateText();
+    }
+    
+
+
+
+
+
+
+
+
+
+
